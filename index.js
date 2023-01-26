914b99bdb48d508ed5f3c0aff84bd1a1709aae19
Marlon Baker <marbaker.swe@gmail.com>
2023-01-25 19:35:59 -0800

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-
 app.get('/', (req, res) => {
-    //name of jsx page 'home.jsx'
     res.render('home')
-  })
-  
+})
+
 app.get('*', (req, res) => {
-    //name of jsx page 'error404.jsx;
     res.render('error404')
-  })
+})
+
+// Listen for Connections
+app.listen(process.env.PORT)
+
+
+
+
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+
+// //Express Settings
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+// app.use('/places', require('./controllers/places'))
+// app.use(express.static('public'))
+
+// //Controllers & Routes
+// app.get('/', (req, res) => {
+//     //name of jsx page 'home.jsx'
+//     res.render('home')
+//   })
   
-  // GET /places
-app.get('/', (req, res) => {
-    res.render('./places/index')
-  })
+// app.get('*', (req, res) => {
+//     //name of jsx page 'error404.jsx;
+//     res.render('error404')
+//   })
+  
+//   // GET /places
+// app.get('/', (req, res) => {
+//     res.render('./places/index')
+//   })
   
 
-app.listen(process.env.PORT)
+// app.listen(process.env.PORT)
