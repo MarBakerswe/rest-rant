e01cbb0fcf087ed31f071c5487b6fb0b8af0c128
Marlon Baker <marbaker.swe@gmail.com>
2023-02-11 14:29:11 -0800

    rest rant 8

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -25,6 +25,11 @@ function new_form () {
                     <input className="form-control" id="state" name="state" />
                 </div>
                 <div className="form-group">
+                    <label for="founded">Founded Year</label>
+                    <input className="form-control" id="founded" name="founded" />
+                    </div>
+
+                <div className="form-group">
                     <label htmlFor="cuisines">Cuisines</label>
                     <input className="form-control" id="cuisines" name="cuisines" required />
                 </div>
